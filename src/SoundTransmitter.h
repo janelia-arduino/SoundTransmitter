90b053e78f9a4411cf2e6788ae226665f59ce9e4
Peter Polidoro <peterpolidoro@gmail.com>
2018-08-12 12:54:07 -0400

    polidorop@janelia.hhmi.org -> peterpolidoro@gmail.com

--- src/SoundTransmitter.h
+++ src/SoundTransmitter.h
@@ -3,7 +3,7 @@
 //
 //
 // Authors:
-// Peter Polidoro polidorop@janelia.hhmi.org
+// Peter Polidoro peterpolidoro@gmail.com
 // ----------------------------------------------------------------------------
 #ifndef SOUND_TRANSMITTER_H
 #define SOUND_TRANSMITTER_H
--- src/SoundTransmitter/5x3.h
+++ src/SoundTransmitter/5x3.h
@@ -3,7 +3,7 @@
 //
 //
 // Authors:
-// Peter Polidoro polidorop@janelia.hhmi.org
+// Peter Polidoro peterpolidoro@gmail.com
 // ----------------------------------------------------------------------------
 #ifndef SOUND_TRANSMITTER_5X3_CONSTANTS_H
 #define SOUND_TRANSMITTER_5X3_CONSTANTS_H
