76b79e661fe5e4bc85675239140696c282a2382a
Peter Polidoro <peterpolidoro@gmail.com>
2020-09-04 16:45:05 -0400

    Update to work with ArduinoJson 6

--- src/SoundTransmitter/SoundTransmitter.cpp
+++ src/SoundTransmitter/SoundTransmitter.cpp
@@ -295,8 +295,8 @@ void SoundTransmitter::setVolume(long volume)
 // floating-point number (float, double)
 // bool
 // const char *
-// ArduinoJson::JsonArray *
-// ArduinoJson::JsonObject *
+// ArduinoJson::JsonArray
+// ArduinoJson::JsonObject
 // const ConstantString *
 //
 // For more info read about ArduinoJson parsing https://github.com/janelia-arduino/ArduinoJson
